a947a5ee0b2244d378a254ebfcaa35c39865ee76
ndelanou <nicolas.delanou@shipfix.com>
2022-09-07 11:07:17 +0200

    fix: backend test env won't launch for tests

--- example/backend/mix.exs
+++ example/backend/mix.exs
@@ -32,9 +32,9 @@ defmodule Backend.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.3"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix, "~> 1.6.12"},
+      {:phoenix_pubsub, "~> 2.0"},
+      {:phoenix_html, "~> 2.14.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
